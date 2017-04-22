b7deeab0c9974ef57e568ecf65faa56a62f05ee1
Mick Lin <cl3469@columbia.edu>
2017-04-22 17:09:38 -0400

    imported read file

---
+++ receiver/FFTreader.hpp
@@ -0,0 +1,26 @@
+#include "aquila/global.h"
+#include "aquila/source/SignalSource.h"
+#include "aquila/transform/FftFactory.h"
+#include "aquila/source/WaveFile.h"
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+class FFTreader
+{
+private:
+	Aquila::WaveFile wav;
+	const std::size_t SIZE = 1024;
+	const std::size_t END;
+	const std::size_t sampleFreq; 
+	vector<int> findMax(Aquila::SpectrumType spectrum);
+	vector<int> freqOfindex(std::size_t start);
+
+public:
+	FFTreader(string file_path): wav(file_path), END(wav.getSamplesCount()),sampleFreq(wav.getSampleFrequency()){}
+	vector<string> parse();
+};

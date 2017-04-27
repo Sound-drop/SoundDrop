29a3692820962bfdf9b78b47dce7fa7ac52dd795
James Young <jtyoung1292@gmail.com>
2017-04-26 21:31:06 -0400

    shell script for portaudio and aquila setup

--- libs/setup.sh
+++ libs/setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "Download and build portaudio..."
-echo "Download and build aqulia..."
+echo "Downloading and building portaudio..."
+curl -sS http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz > portaudio.tgz
+tar -xzvf portaudio.tgz
+rm portaudio.tgz
+cd portaudio
+./configure && make
+cd ..
+
+echo "Downloading and building aqulia..."
+git clone git://github.com/zsiciarz/aquila.git aquila
+cd aquila
+mkdir build
+cd build
+cmake ../../aquila/
+make
+make install
